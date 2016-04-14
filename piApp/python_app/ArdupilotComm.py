59db999cb5b494b3e0be23e14b46a893aa92aa36
Christopher Åkersten <christopher.akersten@gmail.com>
2016-04-14 14:14:11 +0200

    app updated

---
+++ piApp/python_app/ArdupilotComm.py
@@ -0,0 +1,91 @@
+# By Martin and Jacob 2016
+
+
+
+
+#HOW TO USE:
+# from ArdupilotComm import *
+# x = ArdupiloComm()
+# x.sendTargets("
+
+
+
+
+
+import smbus
+address = 0x04
+startChar = '\002'
+endChar = '\003'
+divChar = '|'
+
+
+trackDataid = '1'
+throttlePidid = '2'
+yawPidid = '3'
+rollPidid = '4'
+pitchPidid = '5'
+targetValuesid = '6'
+
+class ArdupilotComm:
+        
+    """For communicating with ardupilot by USB"""
+    bus = smbus.SMBus(1)
+    
+    trackDataid = '1'
+    throttlePidid = '2'
+    yawPidid = '3'
+    rollPidid = '4'
+    pitchPidid = '5'
+    targetValuesid = '6'
+
+    def __init__(self):
+        pass
+
+    def read(self): #Not yet defined how this should work
+        pass
+        
+        #Settings could be PID.ground for ground PID, 2 for angle. 
+    def sendPidSettings(self,pidtype,kp,ki,kd):
+        self.sendString(startChar)  # Indicate start of message
+        
+        self.sendString(pidtype) #Send a number indicating which pid parameters to send. 1 for ground 2 for distance and 3 for angle to user
+        self.sendChar(divChar)
+
+        self.sendString( (kp)+divChar)
+        self.sendString( (ki)+divChar)
+        self.sendString( (kd)+divChar)
+        self.sendChar(endChar)
+
+
+        #Use this to send compassdata and two values for tracking
+    def sendTrackData(self,compassAngle,angleUser,currentHeight,userDistance):
+        self.sendChar(startChar)  # Indicate start of message
+        self.sendString(trackDataid) # indicates that track data will come
+        self.sendChar(divChar)
+        self.sendString(compassAngle)
+        self.sendChar(divChar)
+        self.sendString(angleUser)
+        self.sendChar(divChar)
+        self.sendString(currentHeight)
+        self.sendChar(divChar)
+        self.sendString(userDistance)
+        self.sendChar(divChar)
+        self.sendChar(endChar)
+
+
+    def sendString(self,str):
+        for c in str:
+            self.bus.write_byte(address,int.from_bytes(bytes(c,'utf-8'),'little'))
+            
+    def sendChar(self,charaa):
+        self.bus.write_byte(address,int.from_bytes(bytes(charaa,'utf-8'),'little'))
+
+    def sendTargets(self,targetHeight,targetDistance):
+        self.sendChar(startChar)  # Indicate start of message
+        self.sendChar(divChar)
+        self.sendChar(targetValuesid)
+        self.sendString(targetHeight)
+        self.sendChar(divChar)
+        self.sendString(targetDistance)
+        self.sendChar(divChar)
+        self.sendChar(endChar)
--- piApp/python_app/test.py
+++ piApp/python_app/test.py
@@ -1,14 +1,32 @@
 from Server import ServerManager
 from observer import *
+from ArdupilotComm import ArdupilotComm
 import json
-
+import sys
+import traceback
 
 
 class piApp(Observer):
     def __init__ (self):
-        self.server = ServerManager()
+        self.server = ServerManager(1339)
         self.server.subscribeToServer(self)
         self.server.start()
+        self.aurdoComm = ArdupilotComm()
+        self.sensordata = {
+            'height': "0",
+            'distance': "0",
+            'uavangle': "0",
+            'roll': "0",
+            'pitch': "0",
+            'yaw': "0"};
+    def sendSensorData(self):
+        print("sending tracking data: ")
+        print(self.sensordata)
+        self.aurdoComm.sendTrackData(
+            self.sensordata['yaw'], 
+            self.sensordata['uavangle'], 
+            self.sensordata['height'], 
+            self.sensordata['distance'] )
         
     def notify(self,message,data):                  
         if message == "ReceivedTCP" :
@@ -17,11 +35,32 @@ class piApp(Observer):
                 print ("Received: "+ s)
                 datadict = json.loads(s)
                 print (datadict)
-            except:
-                pass
+                if "settings" in datadict :
+                    print ("received settings")
+                    for key in datadict["settings"] :
+                        print("keytest:"+key)
+                        if key == "pitch" or key == "roll" or key == "trottle" or key == "yaw":
+                            #print ("sending "+key+" values")          
+                            self.aurdoComm.sendPidSettings( {'trottle':'2','roll':'3','yaw':'4','pitch':'5'}[key],
+                                datadict["settings"][key]["p"],
+                                datadict["settings"][key]["i"],
+                                datadict["settings"][key]["d"])
+                        elif key == "target":
+                            self.aurdoComm.sendTargets(
+                                datadict["settings"]["target"]["targetHeight"],
+                                datadict["settings"]["target"]["targetHeight"])
+                if "sensordataoculus" in datadict :
+                    self.sensordata["roll"]=str(datadict["sensordataoculus"]["roll"])
+                    self.sensordata["pitch"]=str(datadict["sensordataoculus"]["pitch"])
+                    self.sensordata["yaw"]=str(datadict["sensordataoculus"]["yaw"])
+                    self.sendSensorData()
+                    
+                    
+            except Exception  as err:
+                print(traceback.format_exc())
+                #or
+                print(sys.exc_info()[0])
                 
-        
-
 if __name__ == "__main__":
 
     #Start App
