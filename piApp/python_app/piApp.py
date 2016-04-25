fe5128b5b6c8cf57a647c36cb90822c48bf23a39
Christopher Åkersten <christopher.akersten@gmail.com>
2016-04-25 16:36:13 +0200

    piapp update

--- piApp/python_app/piApp.py
+++ piApp/python_app/piApp.py
@@ -16,11 +16,15 @@ class piApp(Observer):
         self.calibrated = False
         self.client = None
         self.pinger = Pingers()
+        self.calibrated = False
+        self.dataRequested = False
+        self.calibInProcess = False
         
-        portnr = 1339
+        portnr = 1338
         if len(sys.argv) > 1:
             portnr = int(sys.argv[1]) #Now you can have port as command line argument :)
 
+        self.errorCount = 0
         self.newSettings = False
         self.server = ServerManager(portnr)
         self.server.subscribeToServer(self)
@@ -42,42 +46,43 @@ class piApp(Observer):
         #print("sending tracking data: ")
         #print(self.sensordata)
 
-        while(self.lock):
-            pass
-        self.lock = True
+        #while(self.lock):
+        #    pass
+        #self.lock = True
         self.aurdoComm.sendTrackData(
-            
             self.sensordata['yaw'], 
             self.sensordata['uavangle'], 
             self.sensordata['height'], 
             self.sensordata['distance'] )
-        self.lock = False
+        #self.lock = False
+        #print("i sent you data yo")
         
     def refreshUltraSensors(self):
 
-        print("start get sensor....")
+        #print("start get sensor....")
         while(self.lock):
             time.sleep(0.0000001)
-        print("out of lock1")
+        #print("out of lock1")
         self.lock = True
         self.sensordata['height'] = self.pinger.getHeight()
         self.lock = False
         time.sleep(0.00025)
-        
         #print(self.sensordata['height'])
-        remoteping = self.pinger.getFixedRemote()
+        if self.calibrated:
+            remoteping = self.pinger.getFixedRemote()
+            #print("out of fixedRemote()")
 
-        
-        while(self.lock):
-            time.sleep(0.0000001)
-        print("out of lock2")
-        self.lock = True
-        self.sensordata['distance'] = str(int(remoteping['distError']))
-        self.sensordata['uavangle'] = str(int(remoteping['trackDiff']))
-        #print("trackingdata: " + str(remoteping))
-        self.lock = False
+            
+            #while(self.lock):
+            #    time.sleep(0.0000001)
+            #print("out of lock2")
+            #self.lock = True
+            self.sensordata['distance'] = str(int(remoteping['distError']))
+            self.sensordata['uavangle'] = str(int(remoteping['trackDiff']))
+            #print("trackingdata: " + str(remoteping))
+            #self.lock = False
 
-        print("stop sensor...")
+        #print("stop sensor...")
         
         
     def notify(self,message,data,sender):                  
@@ -91,30 +96,29 @@ class piApp(Observer):
                 #    pass
                 #    print(self.currenttime() - self.requestPingTimestamp)
                 if "requestSensorData" in datadict:
-                    #dictdata = {"sensorData", ""}
-                    #dictdata["sensorData"].update(self.sensordata)
-                    #print("hanindg you stuff")
-                    while(self.lock):
-                        time.sleep(0.0000001)
-                    self.lock = True
-                    dictdata = {"sendordata" : self.sensordata}
-                    self.lock = False
-                    jsondata = json.dumps(dictdata)
-                    #print("HEJ I SEND U YO: "+jsondata)
-                    self.client.request.sendto(jsondata.encode('utf-8'), self.client.client_address)
-                    #print("Sended data " + jsondata)
+                    self.dataRequested = not self.dataRequested
                 if "settings" in datadict :
                     print ("received settings")
                     self.newSettings = True
                     self.settings = datadict
+                if "calibrateSensors" in datadict :
+                    if not self.calibrated and not self.calibInProcess :
+                        self.calibInProcess = True
+                        print("setup stuff")
+                        self.pinger.setup()
+                        self.calibrated = True
+                        self.calibInProcess = False
+                        print("setup done")
+                    
                 if "sensordataoculus" in datadict :
-                    while(self.lock):
-                        time.sleep(0.0000001)
-                    self.lock = True
+                    #while(self.lock):
+                    #    time.sleep(0.0000001)
+                    #self.lock = True
                     self.sensordata["roll"]=str(datadict["sensordataoculus"]["roll"])
                     self.sensordata["pitch"]=str(datadict["sensordataoculus"]["pitch"])
                     self.sensordata["yaw"]=str(datadict["sensordataoculus"]["yaw"])
-                    self.lock = False
+                    #self.lock = False
+                    
                 if "requestVideo" in datadict :
                     videosettings = datadict["requestVideo"]
                     videostring = "raspivid " + \
@@ -135,15 +139,17 @@ class piApp(Observer):
                     videostring = "ps -ef | grep raspivid | grep -v grep | awk '{print $2}' | xargs kill -9"
                     print (videostring);
                     os.system(videostring)
+                    #self.errorCount +=1
+                    #if self.errorCount > 10 :
+                    #    self.client.close = True
                     
             except Exception  as err:
-                #print(traceback.format_exc())
-                #or
-                #print(sys.exc_info()[0])
+                print(traceback.format_exc())
+                print(sys.exc_info()[0])
                 #self.aurdoComm.Sending=False
                 #self.client.close = True
                 #print(data)
-                print("json error")
+                return
         elif message == "ClientConnected" :
             print("basestation stored")
             self.client = data
@@ -161,18 +167,25 @@ if __name__ == "__main__":
     app = piApp()
     while True:
         if app.newSettings :
-            for key in app.settings :    
+            for key in app.settings["settings"] :    
                 if key == "pitch" or key == "roll" or key == "trottle" or key == "yaw":
-                    app.aurdoComm.sendPidSettings( {'trottle':'2','roll':'3','yaw':'4','pitch':'5'}[key],
-                    app.settings["settings"][key]["p"],
-                    app.settings["settings"][key]["i"],
-                    app.settings["settings"][key]["d"])
+                    app.aurdoComm.sendPidSettings( {'trottle':'2','roll':'4','yaw':'3','pitch':'5'}[key], app.settings["settings"][key]["p"], app.settings["settings"][key]["i"], app.settings["settings"][key]["d"])
                 elif key == "target":
-                    app.aurdoComm.sendTargets(
-                    app.settings["settings"]["target"]["targetHeight"],
-                    app.settings["settings"]["target"]["targetDistance"])
+                    app.aurdoComm.sendTargets(app.settings["settings"]["target"]["targetHeight"], app.settings["settings"]["target"]["targetDistance"])
             app.newSettings = False
             print("new settings sent to aurdoino")
             print(app.settings)
+
+        if app.dataRequested :
+            #while(app.lock):
+            #    time.sleep(0.0000001)
+            #app.lock = True
+            dictdata = {"sensordata" : app.sensordata}
+            #app.lock = False
+            jsondata = json.dumps(dictdata)
+            app.client.request.sendto(jsondata.encode('utf-8'), app.client.client_address)
+
         app.refreshUltraSensors()
+        if not app.calibrated:
+            time.sleep(0.06)
         app.sendSensorData()
